bb6381b6d10ad75780e1e4815a2755f80e5b786e
kwakchaewon <74225015+kwakchaewon@users.noreply.github.com>
2022-07-18 02:29:29 +0900

    Update models.py

--- tutorial/community/models.py
+++ tutorial/community/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 # DB에 대한 Model을 정의하며 테이블을 정의하고 관리한다.
-# makemigrations: 바뀐 DB 구조를 확인한다.
-# migrations: 바뀐 스키마를 DB에 반영한다.
+# python manage.py makemigrations community: 테이블 생성
+# python manage.py migrations: DB에 반영한다.
 class Article(models.Model):
     name = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
